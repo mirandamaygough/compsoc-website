8d7bc7036d50f42d683ec6c48f2977e542ef7661
paigeuppal <sc22pu@leeds.ac.uk>
2024-09-01 20:57:30 +0100

    spaced navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav>
-            <div className="flex items-center space-x-4">
+        <nav className='pt-0'>
+          
+            <div className="flex items-center space-x-16">
                 <Link href="/" className="text-4xl font-bold text-center">Home</Link>
                 <Link href="/events" className="text-4xl font-bold text-center">Events</Link>
                 <Link href="/committee" className="text-4xl font-bold text-center">Committee</Link>
                 <Link href="/contact" className="text-4xl font-bold text-center">Contact</Link>
             </div>
+       
         </nav>
     );
 };
