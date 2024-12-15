07abe61b675da134f23ea1bfa39307a5e57a7cce
paigeuppal <sc22pu@leeds.ac.uk>
2024-12-15 18:54:49 +0000

    Logo is svg

--- app/leedshack/page.tsx
+++ app/leedshack/page.tsx
@@ -11,18 +11,15 @@ export default function Leedshack() {
         <div className="flex flex-col min-h-screen">
         <main className="flex-grow flex flex-col items-left space-y-6 p-4 md:p-24 bg-primary text-white">
             <Navbar />
-            {/* <div className="pt-4">
-                <Image src="/images/leedshack-cropped.jpg" alt="LeedsHack" width="482" height="258"/>
-            </div> */}
-            <div className="flex justify-center items-center">
-                 <Image src="/images/leedshack-cropped.jpg" alt="LeedsHack" className = "mt-8 mb-8" width="800" height="428" />
+            <div className="flex items-start">
+                 <Image src="/images/LeedsHackLogo.svg" alt="LeedsHack" className = "mt-8 mb-8" width="800" height="428" />
             </div>
             <p className="text-2xl text-left font-barlow">
                 We&apos;re excited to announce that on February 8-9th, we will be hosting LeedsHack 2025 at the University of Leeds!
             </p>
             <h2 className="text-4xl font-bold text-left mt-4">What is LeedsHack?</h2>
             <p className="text-2xl text-left font-barlow">
-                LeedsHack is a 24-hour hackathon in the Sir William Henry Bragg Building where students from across the UK will come together to innovate, collaborate, and create solutions in the fields of computer science and software engineering.
+                LeedsHack is a 24-hour hackathon in the Sir William Henry Bragg Building at the University of Leeds where students from across the UK will come together to innovate, collaborate, and create solutions in the fields of computer science and software engineering.
                 We will set challenges, as will our sponsors, to give you a starting point for your project. Then it&apos;s up to you to come up with a solution and build it.
                 This is your chance to challenge yourself, learn new skills, connect with like-minded peers, and build your network. It is completely free and food and drink will be provided throughout!
             </p>
