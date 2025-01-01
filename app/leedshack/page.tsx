200c4de39dfa1afff8aa5c87b1e878fc8341ea90
paigeuppal <sc22pu@leeds.ac.uk>
2025-01-01 17:46:17 +0000

    hero added

--- app/leedshack/page.tsx
+++ app/leedshack/page.tsx
@@ -11,8 +11,11 @@ export default function Leedshack() {
         <div className="flex flex-col min-h-screen">
         <main className="flex-grow flex flex-col items-left space-y-6 p-4 md:p-24 bg-primary text-white">
             <Navbar />
-            <div className="flex items-start">
+            {/* <div className="flex items-start">
                  <Image src="/images/LeedsHackLogo.svg" alt="LeedsHack" className = "mt-8 mb-8" width="800" height="428" />
+            </div> */}
+            <div className="flex items-start">
+                 <Image src="/images/leedsHackhero.svg" alt="LeedsHack" className = "mt-8 mb-8" width="1800" height="428" />
             </div>
             <p className="text-2xl text-left font-barlow">
                 We&apos;re excited to announce that on February 8-9th, we will be hosting LeedsHack 2025 at the University of Leeds!
