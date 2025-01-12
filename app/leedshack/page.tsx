a9b0a5f298648d0463ba43ad2812a813699266bb
paigeuppal <sc22pu@leeds.ac.uk>
2025-01-12 18:29:19 +0000

    LeedsHack hero complete

--- app/leedshack/page.tsx
+++ app/leedshack/page.tsx
@@ -14,20 +14,24 @@ export default function Leedshack() {
             {/* <div className="flex items-start">
                  <Image src="/images/LeedsHackLogo.svg" alt="LeedsHack" className = "mt-8 mb-8" width="800" height="428" />
             </div> */}
-            <div className="flex items-start">
-                 <Image src="/images/leedsHackhero.svg" alt="LeedsHack" className = "mt-20" width="1800" height="428" />
+            <div className="flex flex-col md:flex-row items-start justify-center gap-8">
+                    <Image src="/images/LeedsHackLogo.svg" alt="LeedsHack" className="mt-10 mb-8" width="800" height="428" />
+                    <Image src="/images/DateLocation.svg" alt="Date + Location " className="mt-10 mb-8 md:ml-10" width="800" height="428" />
             </div>
-    
-            <div className="flex flex-wrap items-center justify-center gap-x-24 gap-y-24">
-                <img className="max-h-12 w-auto object-contain" src="/images/sponsors/Snowflake_Logo.png" alt="Snowflake" />
-                <img className="max-h-12 w-auto object-contain" src="/images/sponsors/IMDb_logo.png" alt="IMDb" />
+            {/* <div className="flex items-start">
+                 <Image src="/images/leedsHackhero.svg" alt="LeedsHack" className = "mt-12" width="1800" height="428" />
+            </div>
+     */}
+            <div className="flex flex-wrap items-center justify-center gap-x-24 gap-y-24 pt-16">
+                <img className="max-h-20 w-auto object-contain" src="/images/sponsors/Snowflake_Logo.png" alt="Snowflake" />
+                <img className="max-h-20 w-auto object-contain" src="/images/sponsors/IMDb_logo.png" alt="IMDb" />
                 <img className="max-h-12 w-auto object-contain" src="/images/sponsors/Pexip Logo White RGB.svg" alt="IMDb" />
-                <img className="max-h-12 w-auto object-contain" src="/images/sponsors/Hetzner-Logo.jpg" alt="Hetzner" />
-                <img className="max-h-44 w-auto object-contain" src="/images/sponsors/DataCity.png" alt="DataCity" />
-                <img className="max-h-12 w-auto object-contain" src="/images/sponsors/UniOfLeeds.png" alt="Uni of Leeds" />
+                <img className="max-h-10 w-auto object-contain" src="/images/sponsors/hetzner-logo.svg" alt="Hetzner" />
+                <img className="max-h-24 w-auto object-contain" src="/images/sponsors/thedatacitywhite.png" alt="DataCity" />
+                {/* <img className="max-h-12 w-auto object-contain" src="/images/sponsors/UniOfLeeds.png" alt="Uni of Leeds" /> */}
             </div>
   
-            <h2 className="text-4xl font-bold text-left mt-4">What is LeedsHack?</h2>
+            <h2 className="text-4xl font-bold text-left mt-4 pt-12">What is LeedsHack?</h2>
             <p className="text-2xl text-left font-barlow">
                 LeedsHack is a 24-hour hackathon in the Sir William Henry Bragg Building at the University of Leeds where students from across the UK will come together to innovate, collaborate, and create solutions in the fields of computer science and software engineering.
                 We will set challenges, as will our sponsors, to give you a starting point for your project. Then it&apos;s up to you to come up with a solution and build it.
