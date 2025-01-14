42df64303ffb7a9a9c4faa928e9f4674d01be200
Miranda Gough <sc22mg@leeds.ac.uk>
2025-01-14 21:51:40 +0000

    added ticket url redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/leedshack/tickets',
+                destination: 'https://universe.com/leedshack2025',
+                permanent: false,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
